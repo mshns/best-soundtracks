cffcd0f6f64d93566d47c7662026f202da017017
mshns <mishanos412@gmail.com>
2022-11-18 09:29:44 +0300

    feat: add quiz generation

--- soundtracks/scripts/quiz.js
+++ soundtracks/scripts/quiz.js
@@ -36,10 +36,107 @@ translate();
 
 langSelect.addEventListener('change', () => {
   translate();
-  localStorage.setItem("language", langSelect.value)
+  localStorage.setItem("language", langSelect.value);
 });
 
 // quiz
 
-const quizOptions = document.querySelector('.quiz__options')
+const quizOptions = document.querySelector('.options__list');
 
+let level = 0;
+
+function setOptions() {
+  soundtracksData[level].forEach((element, index) => {
+    const li = document.createElement('li');
+    li.classList.add('options__item');
+
+    li.innerHTML = soundtracksData[level][index].nameEn;
+    quizOptions.append(li);
+  });
+}
+setOptions()
+
+
+const quizAnswer = document.querySelector('.quiz__answer');
+
+function setAnswer() {
+  quizAnswer.innerHTML = '';
+
+  const divInfo = document.createElement('div');
+  divInfo.classList.add('answer__info');
+  quizAnswer.append(divInfo);
+
+  const divImg = document.createElement('div');
+  divImg.classList.add('answer__img');
+  divImg.style.backgroundImage = soundtracksData[0][0].image;
+  divInfo.append(divImg);
+
+  const divDetails = document.createElement('div');
+  divDetails.classList.add('answer__details');
+  divInfo.append(divDetails);
+
+  const h2 = document.createElement('h2');
+  h2.classList.add('answer__title');
+  h2.innerHTML = soundtracksData[0][0].nameEn;
+  divDetails.append(h2);
+
+  const trackH3 = document.createElement('h3');
+  trackH3.classList.add('answer__subtitle');
+  trackH3.innerHTML = soundtracksData[0][0].trackEn;
+  divDetails.append(trackH3);
+
+  const authorH3 = document.createElement('h3');
+  authorH3.classList.add('answer__subtitle');
+  authorH3.innerHTML = soundtracksData[0][0].authorEn;
+  divDetails.append(authorH3);
+
+  const divPlayer = document.createElement('div');
+  divPlayer.classList.add('answer__player');
+  divDetails.append(divPlayer);
+
+  const divBtnPlay = document.createElement('div');
+  divBtnPlay.classList.add('answer-button__play');
+  divPlayer.append(divBtnPlay);
+
+  const divTrack = document.createElement('div');
+  divTrack.classList.add('answer__track');
+  divPlayer.append(divTrack);
+
+  const inputProgress = document.createElement('input');
+  inputProgress.classList.add('answer__track');
+  divTrack.append(inputProgress);
+
+  const divControl = document.createElement('div');
+  divControl.classList.add('answer-track__control');
+  divTrack.append(divControl);
+
+  const divVolume = document.createElement('div');
+  divVolume.classList.add('answer-track__volume');
+  divControl.append(divVolume);
+
+  const divBtnVolume = document.createElement('div');
+  divBtnVolume.classList.add('answer-button__volume');
+  divVolume.append(divBtnVolume);
+
+  const inputVolume = document.createElement('input');
+  inputVolume.classList.add('answer-button__level');
+  divVolume.append(inputVolume);
+
+  const divTime = document.createElement('div');
+  divTime.classList.add('answer__time');
+  divControl.append(divTime);
+
+  const divTimeProgress = document.createElement('div');
+  divTimeProgress.classList.add('answer-time__progress');
+  divTime.append(divTimeProgress);
+
+  const divTimeDuration = document.createElement('div');
+  divTimeDuration.classList.add('answer-time__duration');
+  divTime.append(divTimeDuration);
+
+  const description = document.createElement('p');
+  description.classList.add('answer__description');
+  description.innerHTML = soundtracksData[0][0].descriptionEn;
+  quizAnswer.append(description);
+}
+setAnswer();
